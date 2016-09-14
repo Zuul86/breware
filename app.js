aada0c998802363b7f2f6142848c07c53e765074
Adam Pritzl <adam.pritzl@centare.com>
2016-09-14 13:04:53 -0500

    Switch over to pug, added istanbul for coverage

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ sockets(io);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
